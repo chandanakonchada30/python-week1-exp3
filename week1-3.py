3e7c7959abdc352b0d4eb2a1d80b8505f353f2b2
chandanakonchada30 <24331a05e2@mvgrce.edu.in>
2025-12-12 14:11:38 +0530

    Add files via upload

---
+++ week1-3.py
@@ -0,0 +1,12 @@
+#24331A05E2
+#TYPE CONVERSION IN PYTHON
+num1=float(input("Enter a number: "))
+print("value is:", num1)
+print("type is:", type(num1))
+num2=int(num1)
+print("value is:", num2)
+print("type is:", type(num2))
+a="25"
+print("type conversion from string to int:",int(a))
+b="8.59"
+print("type conversion from string to float:",float(b))
